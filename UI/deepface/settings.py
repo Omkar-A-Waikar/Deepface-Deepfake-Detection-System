9a87f8c15ae9cb6dc3a4e4f52b71cf47e18d5943
Omkar Ashok Waikar <ow2130@nyu.edu>
2024-12-04 17:11:27 -0500

    Update settings.py

--- UI/deepface/settings.py
+++ UI/deepface/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'deepface.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "UI" / "templates"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
