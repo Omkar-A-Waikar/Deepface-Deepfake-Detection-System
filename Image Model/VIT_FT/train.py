bbc07000cb20f9b509eadfea75776617a06682d9
Hongdao Meng <108969479+Mycroft-s@users.noreply.github.com>
2024-12-17 20:55:58 -0500

    Update train.py

--- Image Model/VIT_FT/train.py
+++ Image Model/VIT_FT/train.py
@@ -3,23 +3,17 @@ import torch
 from transformers import Trainer, TrainingArguments, default_data_collator
 from Vision_Transformer import CustomImageDataset, load_model
 
-# Set the dataset path and model ID
 img_dir = "F:/FF_Dataset/processed_frames"
 model_id = "google/vit-base-patch16-224-in21k"
-
-# Load the model and feature extractor
+r
 model, feature_extractor = load_model(model_id=model_id)
 
-# Set device (use GPU if available)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
-# Load training and testing datasets
 train_dataset = CustomImageDataset(img_dir=img_dir + "/train", feature_extractor=feature_extractor)
 test_dataset = CustomImageDataset(img_dir=img_dir + "/test", feature_extractor=feature_extractor)
 
-
-# Calculate class weights based on the number of files in each class folder
 def calculate_class_weights_from_folders(train_dir):
     real_dir = os.path.join(train_dir, "REAL")
     fake_dir = os.path.join(train_dir, "FAKE")
@@ -35,11 +29,9 @@ def calculate_class_weights_from_folders(train_dir):
     return torch.tensor([weight_fake, weight_real]).to(device)
 
 
-# Compute class weights
 class_weight = calculate_class_weights_from_folders(img_dir + "/train")
 
 
-# Define Custom Trainer to use weighted loss
 class CustomTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False, **kwargs):
         labels = inputs.pop("labels")
@@ -50,8 +42,6 @@ class CustomTrainer(Trainer):
         return (loss, outputs) if return_outputs else loss
 
 
-
-# Define training parameters with optimized hyperparameters
 training_args = TrainingArguments(
     output_dir="./outputs",
     per_device_train_batch_size=32,  # Adjusted batch size
@@ -68,7 +58,6 @@ training_args = TrainingArguments(
     weight_decay=0.01  # Apply weight decay to reduce overfitting
 )
 
-# Initialize CustomTrainer
 trainer = CustomTrainer(
     model=model,
     args=training_args,
@@ -78,7 +67,6 @@ trainer = CustomTrainer(
 )
 
 print("Start training")
-# Start training
 trainer.train()
 
 # Save the final model
